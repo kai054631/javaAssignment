ec49c759bd6ce58e59bdb6b7ae0625b1ebd34ef9
rayy1218 <raytham1218@gmail.com>
2022-03-12 11:33:52 +0800

    initial commit

---
+++ Game.java
@@ -0,0 +1,60 @@
+//Backend for the 15 puzzle game
+
+import java.util.*;
+
+public class Game {
+    int[][] map;
+    int move;
+    
+    //Constructor to start a game
+    public Game() {
+        map = new int[4][4];
+        move = 0;
+
+        //randomly assign 16 number into map
+        Random rand = new Random();
+        Set<Integer> set = new LinkedHashSet<Integer>();
+        while (set.size() < 16) {
+            set.add(rand.nextInt(16) + 1);
+        }
+        
+        Integer[] setArray = new Integer[set.size()];
+        set.toArray(setArray);
+        for (int i = 0; i < 4; i++) {
+            for (int y = 0; y < 4; y++) {
+                map[i][y] = setArray[i * 4 + y];
+            }
+        }
+    }
+    
+    //Try to move a number in the map return if the move is valid
+    public boolean move(int y, int x) {
+        int toX = -1, toY = -1;
+        if (y > 0 && map[y - 1][x] == 16) {
+            toX = x;
+            toY = y - 1;
+        }
+        else if (y < 3 && map[y + 1][x] == 16) {
+            toX = x;
+            toY = y + 1;
+        } 
+        else if (x > 0 && map[y][x - 1] == 16) {
+            toX = x - 1;
+            toY = y;
+        }
+        else if (x < 3 && map[y][x + 1] == 16) {
+            toX = x + 1;
+            toY = y;
+        }
+
+        if (toX == -1 && toY == -1) {
+            return false;
+        }
+        
+        int temp = map[y][x];
+        map[y][x] = map[toY][toX];
+        map[toY][toX] = temp;
+
+        return true;
+    }
+}
