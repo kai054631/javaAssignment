08a224ffaaf71fd34a4b5525d13ef19157a848b2
kai054631 <88898686+kai054631@users.noreply.github.com>
2022-03-16 22:23:13 +0800

    getWin command update

--- Game.java
+++ Game.java
@@ -56,7 +56,7 @@ public class Game {
         return true;
     }
     public int checkWin(){
-        for(int i=0;i<4;i++)
+        for(int i=0;i<4;i++)//checkWin() just loop through the map arr by row and col return if in order or not
         {
             for(int j=0;j<4;j++)
             {
@@ -65,6 +65,7 @@ public class Game {
             }
             System.out.println(" ");
         }
+        return 0;
     }
     public int[][] getMap(){
         return map;//getMap just return the map 2d arr
